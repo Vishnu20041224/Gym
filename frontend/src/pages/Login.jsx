4c1d85803565eeaf0f58015ad6e683c24519cae9
Vishnu <rhvishnushankar@gmail.com>
2026-01-12 11:10:45 +0530

    Update login API endpoint to production URL

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
     e.preventDefault();
     try {
 
-      const res = await axios.post("http://localhost:5000/api/login",
+      const res = await axios.post("https://vishnu-gym-backend-server.onrender.com/api/login",
         {
           email: form.email,
           password: form.password,
